a33eb480b71575c2321cc0a7829c99750c3a5c7e
daeun <programmerdaeun@gmail.com>
2019-12-06 10:51:55 +0900

    형식 : 코드 정리

--- main.py
+++ main.py
@@ -38,8 +38,6 @@ def main(args):
                     # 미디어 코드 (var pcode = 000)
                     test = soup.find_all('script')
                     match = re.search(r'var pcode = (.*);', test[len(test)-1].get_text())
-                    # print(match)
-                    # print(match.group(0).split("'")[1])
                     mediacode = match.group(0).split("'")[1]
 
                     # -- 데이터 추출 -- 
@@ -53,11 +51,8 @@ def main(args):
                     # (1)(2)(3) 기사 id, title, link
                     for links in soup1.find_all("div", {'class':["bl_topnews_txt", "ns_bl_subnews_title"]}): # - top news + sub news
                         for l in links.find_all("a"):
-                            #print("title: " + l.get_text())
-                            #print("link: " + l.get('href'))
                             title = l.get_text()
                             link = l.get('href')
-                            #print(link)
 
                             # (4) 해당 기사 발행일 : 해당 기사 링크에 들어가서 확인해야함
                             html2 = urlopen(link, context=context)
@@ -67,16 +62,24 @@ def main(args):
 
                             time = soup2.find(class_="publish") or soup2.find(class_="date")
                             published_time = time.get_text()
-                            # print(time.get_text())
 
                             parts = urlparse(link)
                             news_id = parts.path.split('/')[2]
-                            #print("news_id: " + parts.path.split('/')[2])
-             
-                            sql = 'INSERT INTO crawlingDB (id, news_id, title, link, crawling_time, published_time, mediacode) VALUES (%s, %s, %s, %s, now(), %s, %s) ON DUPLICATE KEY UPDATE title = %s'
-                            data = (id, news_id, title, link, published_time, mediacode, title)
-                            curs.execute(sql, data)
-                            conn.commit()
+
+                            # ----- test -----
+                            print("mediacode: " + mediacode)
+                            print("title: " + title)
+                            print("link: " + link)
+                            print("published_time: " + published_time)
+                            print("news_id: "+ news_id)
+                            print("~~~~~")
+                            # ----------------
+
+                            # DB 데이터삽입
+                            # sql = 'INSERT INTO crawlingDB (id, news_id, title, link, crawling_time, published_time, mediacode) VALUES (%s, %s, %s, %s, now(), %s, %s) ON DUPLICATE KEY UPDATE title = %s'
+                            # data = (id, news_id, title, link, published_time, mediacode, title)
+                            # curs.execute(sql, data)
+                            # conn.commit()
 
                             # interval 5분으로 돌려보기
                             # sql = 'DELETE FROM crawlingDB WHERE crawling_time <= DATE_SUB(now(), INTERVAL 5 MINUTE)'
