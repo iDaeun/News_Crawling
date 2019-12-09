5db3726a353e9fa2a332043b569f93f6166d6a2b
daeun <programmerdaeun@gmail.com>
2019-12-09 15:04:21 +0900

    기능 : 난수(id) -> 매 기사마다 다르게 생성

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main(args):
                     conn = pymysql.connect(host=TargetConfig.DB_HOST, user=TargetConfig.DB_USER, password=TargetConfig.DB_PW, db=TargetConfig.DB_NAME, charset='utf8')
                     curs = conn.cursor()
 
-                    id = str(uuid.uuid4())
+                    
 
                     # 미디어 코드 (var pcode = 000)
                     test = soup.find_all('script')
@@ -52,6 +52,9 @@ def main(args):
                     # (1)(2)(3) 기사 id, title, link
                     for links in soup1.find_all("div", {'class':["bl_topnews_txt", "ns_bl_subnews_title"]}): # - top news + sub news
                         for l in links.find_all("a"):
+
+                            id = str(uuid.uuid4())
+
                             title = l.get_text()
                             link = l.get('href')
 
@@ -62,6 +65,7 @@ def main(args):
                             getTime = time["content"]
                             parsed = parse(getTime)
                             d = parsed.date().strftime('%Y-%m-%d')
+                            # 만약 시간이 없으면 -> 00:00으로 출력
                             t = parsed.time().strftime('%H:%M:%S')
                             published_time = d + " " + t
 
@@ -78,10 +82,10 @@ def main(args):
                             # ----------------
 
                             # DB 데이터삽입
-                            # sql = 'INSERT INTO crawlingDB (id, news_id, title, link, crawling_time, published_time, mediacode) VALUES (%s, %s, %s, %s, now(), %s, %s) ON DUPLICATE KEY UPDATE title = %s'
-                            # data = (id, news_id, title, link, published_time, mediacode, title)
-                            # curs.execute(sql, data)
-                            # conn.commit()
+                            sql = 'INSERT INTO crawlingDB (id, news_id, title, link, crawling_time, published_time, mediacode) VALUES (%s, %s, %s, %s, now(), %s, %s) ON DUPLICATE KEY UPDATE title = %s'
+                            data = (id, news_id, title, link, published_time, mediacode, title)
+                            curs.execute(sql, data)
+                            conn.commit()
 
                             # interval 5분으로 돌려보기
                             # sql = 'DELETE FROM crawlingDB WHERE crawling_time <= DATE_SUB(now(), INTERVAL 5 MINUTE)'
