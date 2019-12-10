cd889bf7e221931e2c9186a0a2a7d15c54fc49a5
daeun <programmerdaeun@gmail.com>
2019-12-10 14:21:43 +0900

    기능 : 언론사 별로 pcode매칭하여 dictionary생성

--- getPcode.py
+++ getPcode.py
@@ -16,27 +16,32 @@ def main():
     source = html.read()
     html.close()
 
+    mediacode = []
+
     try:
         soup = BeautifulSoup(source, "html5lib")
         script = soup.find_all('script')
         myScript = script[len(script)-1]
 
-        # pcode 추출
-        pid = re.search(r'headlineList : (.*)', myScript.get_text()) 
-        pgrp = pid.group(1).split('"')
-
-        #for r in range(3,len(pgrp)-5,+2):
-            #print(pgrp[r])
-
-        # 언론사명 추출
+        # 언론사명 추출 1
         pressCategory = re.search(r'"ct1":(.*)', myScript.get_text())
         myCate = pressCategory.group(1)
-        #print(myCate.find('032').get_text())
-        print(re.search(r'"pid":"930","name":"(.*?)"', myCate))
-        tt = re.search(r'"pid":"930","name":"(.*?)"', myCate)
-        print(tt.group().split('"'))
-        ttt = tt.group().split('"')
-        print(ttt[7])
+
+        # pcode 추출
+        pid = re.search(r'headlineList : (.*)', myScript.get_text()) 
+        pgrp = pid.group(1).split('"') #print(myCate.find('032').get_text())
+
+        for r in range(3,len(pgrp)-5,+2):
+            print(" - key값 : "+pgrp[r])
+
+            # 언론사명 추출 2 '+pgrp[r]+'
+            c1 = re.search(r'"pid":"' + pgrp[r] + '","name":"(.*?)"', myCate)
+            c2 = c1.group().split('"')
+            print(" - value값 : "+c2[7])
+
+            dic = {pgrp[r]:c2[7]}
+            print(dic)
+            print("~~~~~~~~~~~")
 
     except:
         logging.error("main error(2)")
