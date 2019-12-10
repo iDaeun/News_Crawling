333b43479b42bfe5c237c46e3ccb1faf63650724
daeun <programmerdaeun@gmail.com>
2019-12-10 13:55:15 +0900

    기능 : 언론사이름 추출

--- getPcode.py
+++ getPcode.py
@@ -20,15 +20,23 @@ def main():
         soup = BeautifulSoup(source, "html5lib")
         script = soup.find_all('script')
         myScript = script[len(script)-1]
-        
-        #pressCategory = re.search(r'"ct1":(.*)', myScript.get_text())
 
         # pcode 추출
         pid = re.search(r'headlineList : (.*)', myScript.get_text()) 
         pgrp = pid.group(1).split('"')
 
-        for r in range(3,len(pgrp)-5,+2):
-            print(pgrp[r])
+        #for r in range(3,len(pgrp)-5,+2):
+            #print(pgrp[r])
+
+        # 언론사명 추출
+        pressCategory = re.search(r'"ct1":(.*)', myScript.get_text())
+        myCate = pressCategory.group(1)
+        #print(myCate.find('032').get_text())
+        print(re.search(r'"pid":"930","name":"(.*?)"', myCate))
+        tt = re.search(r'"pid":"930","name":"(.*?)"', myCate)
+        print(tt.group().split('"'))
+        ttt = tt.group().split('"')
+        print(ttt[7])
 
     except:
         logging.error("main error(2)")
