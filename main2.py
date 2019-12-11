278c96fc6903d766a345d2c9caa7d48190af1035
daeun <programmerdaeun@gmail.com>
2019-12-11 17:15:55 +0900

    기능 : 전체 언론사에 적용되는 코드로 수정
    -  '기사 제목'과 기타(부제목, 기사요약) 구분
       1차 구분 ) "_blank"태그로 가져온 텍스트 길이 -> 0이상, 100이하로 제한
       2차 구분 ) DB삽입 시, 아이디(링크 뒷부분)을 기준으로 중복하면 새로운 데이터로 업데이트
    - 난수 발생은 기사제목 여부 구분 후 실행되도록 조정
    - 모든 언론사 스탠드 탭은 iframe으로 구성되어 있음 : 언론사별로 나뉘는 뒷부분은 DB에 저
      장된 데이터를 붙여 url완성시킴

--- main2.py
+++ main2.py
@@ -33,82 +33,89 @@ def main(args):
             try:
                 # loop = True
                 # while loop == True: 
-                    soup = openurl(TargetConfig.MAIN)
 
-                    # DB저장
+                    # DB사용
                     conn = pymysql.connect(host=TargetConfig.DB_HOST, user=TargetConfig.DB_USER, password=TargetConfig.DB_PW, db=TargetConfig.DB_NAME, charset='utf8')
                     curs = conn.cursor()
 
-                    # -- 데이터 추출 -- 
-                    # iframe으로 되어있음
-                    soup1 = openurl(TargetConfig.IFRAME)
+                    sql = 'SELECT mediacode FROM newsList'
+                    curs.execute(sql)
+                    cateList = curs.fetchall()
                     
-                    # 모든 기사제목 가져옴
-                    for bl in soup1.find_all('a', target="_blank"):
-                        #if len(bl.get_text()) < 1:
-                            #print(bl.find_all("img", alt=True))
-                            # blo = bl.find_all("img", alt=True)
-                            # for blot in blo:
-                            #     print(blot["alt"])
-                            #     print("------------")
-                        #print(bl.find_parent('div'))
-                        #print(bl.parent('div'))
-                        #print("--------------------")
+                    # 1. 미디어코드 [mediacode] 
+                    for cate in cateList:
+                        print("-- newsList에서 하나씩 가져오기 : " + cate[0])
+                        bl_mediacode = cate[0]
 
-                        # 1. 아이디 [id]
-                        id = str(uuid.uuid4())
+                        # -- 데이터 추출 -- 
+                        # 미디어 코드 대입, iframe으로 되어있음
+                        addUrl = TargetConfig.IFRAME + cate[0] + '.html'
+                        print("-- IFRAME 주소 만들기 : " + addUrl)
+                        soup1 = openurl(addUrl)
                         
-                        # 2. 제목 [title]
-                        bl_title = bl.get_text().strip()
-
-                        if len(bl_title) > 0:
-                            print("제목 : " + bl_title)
-
-                            # 3. 링크 [link]
-                            bl_link = bl.get('href')
-                            print("링크 : " + bl_link)
-
-                            # 4. duplicate on (sql) 기준으로 잡을 기사 아이디 [news_id]
-                            parts = bl_link.split('/')
-                            news_id = parts[len(parts)-1]
-                            print("아이디 : " + news_id)
-
-                            soup3 = openurl(bl_link)
-
-                            # 5. 발행 시간 [published_time]
-                            if soup3.find("meta", property="article:published_time"):
-
-                                bl_time = soup3.find("meta", property="article:published_time")
-                                bl_getTime = bl_time["content"]
-                                bl_parsed = parse(bl_getTime)
-                                bl_d = bl_parsed.date().strftime('%Y-%m-%d')
-                                # 만약 시간이 없으면 -> 00:00으로 출력
-                                bl_t = bl_parsed.time().strftime('%H:%M:%S')
-                                bl_published_time = bl_d + " " + bl_t
-
-                                print("시간 : " + bl_published_time)
+                        # 모든 기사제목 가져옴
+                        for bl in soup1.find_all('a', target="_blank"):
                             
-                            else:
-                                print("시간 : -- none -- ")
+                            # 3. 제목 [title]
+                            bl_title = bl.get_text().strip()
+                            
+                            if len(bl_title) > 0 and len(bl_title) <= 50:
 
-                            # 6. 카테고리 [category]
-                            # 7. 미디어코드 [mediacode]                            
+                                print(len(bl_title))
 
-                            print("--------------------")
+                                # 2. 아이디 [id]
+                                id = str(uuid.uuid4())
+                                print("난수 생성 : " + id)
 
-                            # # DB 데이터삽입
-                            # sql = 'INSERT INTO crawlingDB (id, news_id, title, link, crawling_time, published_time, mediacode) VALUES (%s, %s, %s, %s, now(), %s, %s) ON DUPLICATE KEY UPDATE title = %s'
-                            # data = (id, news_id, title, link, published_time, mediacode, title)
-                            # curs.execute(sql, data)
-                            # conn.commit()
+                                print("제목 : " + bl_title)
 
-                            # # interval 1분으로 돌려보기
-                            # sql = 'DELETE FROM crawlingDB WHERE crawling_time <= DATE_SUB(now(), INTERVAL 1 MINUTE)'
-                            # result = curs.execute(sql)
-                            # if result > 0:
-                            #     print("@@@@@@ deleted @@@@@@@")
-                            # conn.commit()
-                        
+                                # 4. 링크 [link]
+                                bl_link = bl.get('href')
+                                print("링크 : " + bl_link)
+
+                                # 5. duplicate on (sql) 기준으로 잡을 기사 아이디 [news_id]
+                                parts = bl_link.split('/')
+                                bl_news_id = parts[len(parts)-1]
+                                print("아이디 : " + bl_news_id)
+
+                                soup3 = openurl(bl_link)
+
+                                # 6. 발행 시간 [published_time]
+                                if soup3.find("meta", property="article:published_time"):
+
+                                    bl_time = soup3.find("meta", property="article:published_time")
+                                    bl_getTime = bl_time["content"]
+                                    bl_parsed = parse(bl_getTime)
+                                    bl_d = bl_parsed.date().strftime('%Y-%m-%d')
+                                    # 만약 시간이 없으면 -> 00:00으로 출력
+                                    bl_t = bl_parsed.time().strftime('%H:%M:%S')
+                                    bl_published_time = bl_d + " " + bl_t
+
+                                    print("시간 : " + bl_published_time)
+                                
+                                else:
+                                    bl_published_time = '00:00'
+                                    print("시간 : -- none -- ")
+
+                                # 7. 카테고리 [category]
+
+                                print("--------------------")
+
+                                # DB 데이터삽입
+                                sql = 'INSERT INTO crawlingDB (id, news_id, title, link, crawling_time, published_time, mediacode) VALUES (%s, %s, %s, %s, now(), %s, %s) ON DUPLICATE KEY UPDATE title = %s'
+                                data = (id, bl_news_id, bl_title, bl_link, bl_published_time, bl_mediacode, bl_title)
+                                curs.execute(sql, data)
+                                conn.commit()
+
+                                # # interval 1분으로 돌려보기
+                                # sql = 'DELETE FROM crawlingDB WHERE crawling_time <= DATE_SUB(now(), INTERVAL 1 MINUTE)'
+                                # result = curs.execute(sql)
+                                # if result > 0:
+                                #     print("@@@@@@ deleted @@@@@@@")
+                                # conn.commit()
+                    
+                    curs.close()
+
                     # sleep(30)
 
             except:
